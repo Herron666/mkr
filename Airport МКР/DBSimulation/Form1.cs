0171f29271bb6d75dfaffcf45dabca442ac9a1ba
Herron666 <74177897+Herron666@users.noreply.github.com>
2021-05-24 10:52:32 +0300

    Add files via upload

---
+++ Airport МКР/DBSimulation/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DBSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Show(_db.GetAll());
+        }
+
+        private PcDatabase _db = new PcDatabase();
+        
+
+        private void Show(List<PC> list)
+        {
+            if (list.Count == 0)
+            {
+                MessageBox.Show("Введіть дані ");
+                return;
+            }
+            dataGridView1.RowCount = list.Count;
+            for (int i=0;i<list.Count;i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = list[i].flight_number;
+                dataGridView1.Rows[i].Cells[1].Value = list[i].City_of_arrival;
+                dataGridView1.Rows[i].Cells[2].Value = list[i].Number_of_seats;
+                dataGridView1.Rows[i].Cells[3].Value = list[i].flight_time;
+                dataGridView1.Rows[i].Cells[4].Value = list[i].ticket_price;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PC newPc = new PC(
+                double.Parse(textBoxName.Text),
+                textBoxWork.Text,
+                double.Parse(textBoxNumber1.Text),
+                double.Parse(textBoxNumber2.Text),
+                double.Parse(textBoxPrice.Text)
+                );
+            _db.Add(newPc);
+            Show(_db.GetAll());
+            tabControl1.SelectedIndex = 0;
+            StreamWriter sw = new StreamWriter("text.txt", true);
+            sw.WriteLine(textBoxName.Text);
+            sw.WriteLine(textBoxWork.Text);
+            sw.WriteLine(textBoxNumber1.Text);
+            sw.WriteLine(textBoxNumber2.Text);
+            sw.WriteLine(textBoxPrice.Text);
+            sw.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.CurrentCell.RowIndex;
+            PC PcToDelete = _db.GetByIndex(i);
+            _db.Remove(PcToDelete);
+            Show(_db.GetAll());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Airport МКР/DBSimulation/PC.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class PC
+    {
+
+        private double _flight_number;
+        public double flight_number
+        {
+            get
+            {
+                return _flight_number;
+            }
+            set
+            {
+                if (value >= 0)
+                    _flight_number = value;
+                else
+                    throw new Exception("Номер рейсу не може бути відємним");
+            }
+        }
+
+        public string City_of_arrival { get; set; }
+
+        private double _Number_of_seats;
+        public double Number_of_seats
+        {
+            get
+            {
+                return _Number_of_seats;
+            }
+            set
+            {
+                if (value >= 0)
+                    _Number_of_seats = value;
+                else
+                    throw new Exception("Номер місця не може бути відємним");
+            }
+        }
+
+
+
+        private double _flight_time;
+        public double flight_time
+        {
+            get
+            {
+                return _flight_time;
+            }
+            set
+            {
+                if (value >= 0)
+                    _flight_time = value;
+                else
+                    throw new Exception("Час перельоту не може бути відємним");
+            }
+        }
+
+        private double _ticket_price;
+        public double ticket_price
+        {
+            get
+            {
+                return _ticket_price;
+            }
+            set
+            {
+                if (value >= 0)
+                    _ticket_price = value;
+                else
+                    throw new Exception("Ціна перельоту не може бути відємним");
+            }
+        }
+
+
+
+        public PC(double flight_number, string City_of_arrival, double Number_of_seats, double flight_time, double ticket_price)
+        {
+            this.flight_number = flight_number;
+            this.City_of_arrival = City_of_arrival;
+            this.Number_of_seats = Number_of_seats;
+            this.flight_time = flight_time;
+            this.ticket_price = ticket_price;
+
+        }
+
+    }
+}
